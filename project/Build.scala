58f760ce837f04c9ef7e5268331065e49323dd5d
Niklas Klein <my.taig@gmail.com>
2014-07-31 14:17:16 +0200

    Moved min & target sdk version definition from manifest to build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -1,14 +1,17 @@
 import sbt._
 import sbt.Keys._
+import android.Keys._
 
 object Build extends android.AutoBuild
 {
 	lazy val main = Project( "android-toolbelt", file( "." ) )
 		.settings(
+			minSdkVersion in Android := "10",
 			name := "android-toolbelt",
 			organization := "com.taig.android",
 			scalaVersion := "2.11.2",
-			scalacOptions ++= Seq( "-feature", "-language:implicitConversions" ), 
+			scalacOptions ++= Seq( "-feature", "-language:implicitConversions", "-deprecation" ), 
+			targetSdkVersion in Android := "20",
 			version := "1.0.0"
 		)
 }
