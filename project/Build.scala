56ab0f1c654cdec63df1b3dc895bf48fbb9290f5
Niklas Klein <my.taig@gmail.com>
2014-08-03 00:44:52 +0200

    Reordered settings

--- project/Build.scala
+++ project/Build.scala
@@ -6,12 +6,12 @@ object Build extends android.AutoBuild
 {
 	lazy val main = Project( "android-toolbelt", file( "." ) )
 		.settings(
-			minSdkVersion in Android := "10",
 			name := "android-toolbelt",
 			organization := "com.taig.android",
 			scalaVersion := "2.11.2",
 			scalacOptions ++= Seq( "-feature", "-language:implicitConversions", "-deprecation" ), 
-			targetSdkVersion in Android := "20",
-			version := "1.0.0"
+			version := "1.0.0",
+			minSdkVersion in Android := "10",
+			targetSdkVersion in Android := "20"
 		)
 }
