31e239733ace5856314f8cb3470e135523cfa8f1
Niklas Klein <my.taig@gmail.com>
2014-08-10 10:53:20 +0200

    Added method readParcelable to RichParcel that omits classloader parameter

--- src/main/scala/com/taig/android/parcel/RichParcel.scala
+++ src/main/scala/com/taig/android/parcel/RichParcel.scala
@@ -1,10 +1,18 @@
 package com.taig.android.parcel
 
 import android.os.Parcel
+import android.{ os => android }
+
+import scala.reflect.ClassTag
 
 class RichParcel( parcel: Parcel )
 {
 	def writeBoolean( boolean: Boolean ) = parcel.writeValue( boolean )
 
 	def readBoolean() = parcel.readValue( classOf[Boolean].getClassLoader ).asInstanceOf[Boolean]
+
+	def readParcelable[T <: android.Parcelable: ClassTag] =
+	{
+		parcel.readParcelable[T]( implicitly[ClassTag[T]].runtimeClass.getClassLoader )
+	}
 }
