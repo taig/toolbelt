23454ef9d2097efd3b3259987ba4a5e68ce48629
Niklas Klein <my.taig@gmail.com>
2014-10-10 23:54:18 +0200

    Code formatting

--- src/main/scala/com/taig/android/serialization/package.scala
+++ src/main/scala/com/taig/android/serialization/package.scala
@@ -10,31 +10,29 @@ package object serialization
 	{
 		def writeBoolean( boolean: Boolean ) = parcel.writeValue( boolean )
 
-		def readBoolean() =
-		{
-			parcel.readValue( classOf[Boolean].getClassLoader ).asInstanceOf[Boolean]
-		}
+		def readBoolean = parcel
+			.readValue( classOf[Boolean].getClassLoader )
+			.asInstanceOf[Boolean]
 
 		def writeOption[T]( option: Option[T] ) = parcel.writeValue( option.orNull )
 
-		def readOption[T: ClassTag] =
-		{
-			Option( parcel.readValue( implicitly[ClassTag[T]].runtimeClass.getClassLoader ).asInstanceOf[T] )
-		}
-
-		def readParcel[T <: android.os.Parcelable: ClassTag] =
-		{
-			parcel.readParcelable[T]( implicitly[ClassTag[T]].runtimeClass.getClassLoader )
-		}
-
-		def readParcelArray[T <: android.os.Parcelable: ClassTag] =
+		def readOption[T: ClassTag] = Option
 		{
 			parcel
-				.readParcelableArray( implicitly[ClassTag[T]].runtimeClass.getClassLoader )
-				.map( _.asInstanceOf[T] )
+				.readValue( implicitly[ClassTag[T]].runtimeClass.getClassLoader )
+				.asInstanceOf[T]
 		}
 
-		def readSerialized[T] = parcel.readSerializable.asInstanceOf[T]
+		def readParcel[T <: android.os.Parcelable: ClassTag] = parcel
+			.readParcelable[T]( implicitly[ClassTag[T]].runtimeClass.getClassLoader )
+
+		def readParcelArray[T <: android.os.Parcelable: ClassTag] = parcel
+			.readParcelableArray( implicitly[ClassTag[T]].runtimeClass.getClassLoader )
+			.map( _.asInstanceOf[T] )
+
+		def readSerialized[T] = parcel
+			.readSerializable
+			.asInstanceOf[T]
 	}
 
 	implicit def `Function1 -> T -> T`[T <: Object: ClassTag]( f: Parcel => T ) = new android.os.Parcelable.Creator[T]
