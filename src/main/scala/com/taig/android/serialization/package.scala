7e94e00d11f533a32bc079705180f4f94019f832
Niklas Klein <my.taig@gmail.com>
2014-10-10 23:50:04 +0200

    Added parcel write & read Option

---
+++ src/main/scala/com/taig/android/serialization/package.scala
@@ -0,0 +1,46 @@
+package com.taig.android
+
+import android.os.Parcel
+
+import scala.reflect.ClassTag
+
+package object serialization
+{
+	implicit class RichParcel( parcel: Parcel )
+	{
+		def writeBoolean( boolean: Boolean ) = parcel.writeValue( boolean )
+
+		def readBoolean() =
+		{
+			parcel.readValue( classOf[Boolean].getClassLoader ).asInstanceOf[Boolean]
+		}
+
+		def writeOption[T]( option: Option[T] ) = parcel.writeValue( option.orNull )
+
+		def readOption[T: ClassTag] =
+		{
+			Option( parcel.readValue( implicitly[ClassTag[T]].runtimeClass.getClassLoader ).asInstanceOf[T] )
+		}
+
+		def readParcel[T <: android.os.Parcelable: ClassTag] =
+		{
+			parcel.readParcelable[T]( implicitly[ClassTag[T]].runtimeClass.getClassLoader )
+		}
+
+		def readParcelArray[T <: android.os.Parcelable: ClassTag] =
+		{
+			parcel
+				.readParcelableArray( implicitly[ClassTag[T]].runtimeClass.getClassLoader )
+				.map( _.asInstanceOf[T] )
+		}
+
+		def readSerialized[T] = parcel.readSerializable.asInstanceOf[T]
+	}
+
+	implicit def `Function1 -> T -> T`[T <: Object: ClassTag]( f: Parcel => T ) = new android.os.Parcelable.Creator[T]
+	{
+		override def createFromParcel( source: Parcel ): T = f( source )
+
+		override def newArray( size: Int ) = new Array[T]( size )
+	}
+}
