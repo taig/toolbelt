49b01b20c1c50ad332726271d96e84c2f885faa3
Niklas Klein <my.taig@gmail.com>
2014-10-15 00:45:42 +0200

    Save and restore InlineEditText state

--- src/main/scala/com/taig/android/widget/InlineEditText.scala
+++ src/main/scala/com/taig/android/widget/InlineEditText.scala
@@ -1,10 +1,12 @@
 package com.taig.android.widget
 
 import android.content.Context
-import android.util.AttributeSet
+import android.os.{Bundle, Parcelable}
+import android.util.{SparseArray, AttributeSet}
 import android.view.View
 import android.widget.{TextView, EditText, ViewSwitcher}
 import com.taig.android.graphic.Color
+import com.taig.android.widget.InlineEditText.Parameter
 
 /**
  * A widget that can switch between an [[android.widget.EditText]] and a [[android.widget.TextView]]
@@ -54,5 +56,46 @@ class InlineEditText( context: Context, attributes: AttributeSet ) extends ViewS
 		}
 
 		super.setDisplayedChild( whichChild )
+	}
+
+	override def onSaveInstanceState() =
+	{
+		val bundle = new Bundle()
+		bundle.putParcelable( Parameter.Instance, InlineEditText.super.onSaveInstanceState )
+		bundle.putParcelable( Parameter.Edit, edit.onSaveInstanceState() )
+		bundle.putParcelable( Parameter.Text, text.onSaveInstanceState() )
+		bundle.putInt( Parameter.Current, getDisplayedChild )
+
+		bundle
+	}
+
+	override def dispatchSaveInstanceState( container: SparseArray[Parcelable] ) = super.dispatchFreezeSelfOnly( container )
+
+	override def onRestoreInstanceState( state: Parcelable ) = state match
+	{
+		case bundle: Bundle =>
+		{
+			super.onRestoreInstanceState( bundle.getParcelable( Parameter.Instance ) )
+			edit.onRestoreInstanceState( bundle.getParcelable( Parameter.Edit ) )
+			text.onRestoreInstanceState( bundle.getParcelable( Parameter.Text ) )
+			setDisplayedChild( bundle.getInt( Parameter.Current ) )
+		}
+		case _ => super.onRestoreInstanceState( state )
+	}
+
+	override def dispatchRestoreInstanceState( container: SparseArray[Parcelable] ) = super.dispatchThawSelfOnly( container )
+}
+
+object InlineEditText
+{
+	val Parameter = new
+	{
+		val Current = getClass.getName + ".Current"
+
+		val Edit = getClass.getName + ".Current"
+
+		val Instance = getClass.getName + ".Instance"
+
+		val Text = getClass.getName + ".Current"
 	}
 }
