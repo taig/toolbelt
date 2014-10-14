1d653b4825db2745101d0e1999f31e5e8c7afcde
Niklas Klein <my.taig@gmail.com>
2014-10-14 23:24:56 +0200

    Added InlineEditText widget

--- src/main/scala/com/taig/android/widget/package.scala
+++ src/main/scala/com/taig/android/widget/package.scala
@@ -1,13 +1,20 @@
 package com.taig.android
 
 import android.view.inputmethod.EditorInfo
-import android.widget.{EditText, TextView}
+import android.widget.{ViewSwitcher, EditText, TextView}
 import com.taig.android.conversion._
 
 package object widget
 {
 	implicit class RichTextView( view: TextView )
 	{
+		/**
+		 * A callback to be triggered when the software keyboard 'DONE' button is clicked while this TextView has
+		 * focus
+		 * 
+		 * @param listener The listener to trigger
+		 * @see [[android.widget.TextView.OnEditorActionListener]]
+		 */
 		def setOnEditorDoneListener( listener: Text.Listener.OnEditorDone ) =
 		{
 			view.setOnEditorActionListener( ( view: EditText, action: Int ) => action & EditorInfo.IME_MASK_ACTION match
@@ -16,5 +23,10 @@ package object widget
 				case _ => false
 			} )
 		}
+	}
+
+	implicit class RichViewSwitcher( view: ViewSwitcher )
+	{
+		def toggle() = view.setDisplayedChild( ( view.getDisplayedChild + 1 ) % 2 )
 	}
 }
