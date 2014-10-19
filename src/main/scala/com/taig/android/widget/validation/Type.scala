b5ed106cf4b2fe95f33df36cc68c39ab87dfbe43
Niklas Klein <my.taig@gmail.com>
2014-10-19 13:05:08 +0200

    Fixed var message override

--- src/main/scala/com/taig/android/widget/validation/Type.scala
+++ src/main/scala/com/taig/android/widget/validation/Type.scala
@@ -4,57 +4,57 @@ import android.util.Patterns
 
 object Type
 {
-	class Alpha( enabled: Boolean, message: CharSequence ) extends Validator.Regex( enabled, message, "[\\p{L}]*" )
+	class Alpha( enabled: Boolean, template: CharSequence ) extends Validator.Regex( enabled, template, "[\\p{L}]*" )
 
-	class AlphaDash( enabled: Boolean, message: CharSequence ) extends Validator.Regex( enabled, message, "[\\p{L}\\-]*" )
+	class AlphaDash( enabled: Boolean, template: CharSequence ) extends Validator.Regex( enabled, template, "[\\p{L}\\-]*" )
 
-	class AlphaNumeric( enabled: Boolean, message: CharSequence ) extends Validator.Regex( enabled, message, "[\\p{L}0-9]*" )
+	class AlphaNumeric( enabled: Boolean, template: CharSequence ) extends Validator.Regex( enabled, template, "[\\p{L}0-9]*" )
 
-	class Email( enabled: Boolean, message: CharSequence ) extends Validator.Regex( enabled, message, Patterns.EMAIL_ADDRESS.pattern() )
+	class Email( enabled: Boolean, template: CharSequence ) extends Validator.Regex( enabled, template, Patterns.EMAIL_ADDRESS.pattern() )
 
-	class Integer( enabled: Boolean, message: CharSequence ) extends Validator.Regex( enabled, message, "[0-9]*" )
+	class Integer( enabled: Boolean, template: CharSequence ) extends Validator.Regex( enabled, template, "[0-9]*" )
 
-	class Length( enabled: Boolean, message: CharSequence, var value: Int ) extends Validator( enabled, message )
+	class Length( enabled: Boolean, template: CharSequence, var value: Int ) extends Validator( enabled, template )
 	{
-		def this( message: CharSequence, value: Int ) = this( value != -1, message, value )
+		def this( template: CharSequence, value: Int ) = this( value != -1, template, value )
 
-		override def getMessage = message.toString.format( value )
+//		override def message = template.toString.format( value )
 
 		override def validate( value: CharSequence ) = super.validate( value ) || value.length() == this.value
 	}
 
-	abstract class Matches( enabled: Boolean, message: CharSequence, var target: Int ) extends Validator( enabled, message )
+	abstract class Matches( enabled: Boolean, template: CharSequence, var target: Int ) extends Validator( enabled, template )
 	{
-		def this( message: CharSequence, target: Int ) = this( target > 0, message, target )
+		def this( template: CharSequence, target: Int ) = this( target > 0, template, target )
 
 		override def validate( value: CharSequence ) = super.validate( value ) || value.toString == find.toString
 
 		protected def find: CharSequence
 	}
 
-	class Max( enabled: Boolean, message: CharSequence, var length: Int ) extends Validator( enabled, message )
+	class Max( enabled: Boolean, template: CharSequence, var length: Int ) extends Validator( enabled, template )
 	{
-		def this( message: CharSequence, length: Int ) = this( length < Int.MaxValue, message, length )
+		def this( template: CharSequence, length: Int ) = this( length < Int.MaxValue, template, length )
 
-		override def getMessage = message.toString.format( length )
+//		override def message = template.toString.format( length )
 
 		override def validate( value: CharSequence ) = super.validate( value ) || value.length() <= length
 	}
 
-	class Min( enabled: Boolean, message: CharSequence, var length: Int ) extends Validator( enabled, message )
+	class Min( enabled: Boolean, template: CharSequence, var length: Int ) extends Validator( enabled, template )
 	{
-		def this( message: CharSequence, length: Int ) = this( length > Int.MinValue, message, length )
+		def this( template: CharSequence, length: Int ) = this( length > Int.MinValue, template, length )
 
-		override def getMessage = message.toString.format( length )
+//		override def message = template.toString.format( length )
 
 		override def validate( value: CharSequence ) = super.validate( value ) || value.length() >= length
 	}
 
-	class Numeric( enabled: Boolean, message: CharSequence ) extends Validator.Regex( enabled, message, "\\d*([\\.,]\\d+)?" )
+	class Numeric( enabled: Boolean, template: CharSequence ) extends Validator.Regex( enabled, template, "\\d*([\\.,]\\d+)?" )
 
-	class Phone( enabled: Boolean, message: CharSequence ) extends Validator.Regex( enabled, message, "(\\+?\\d+)?" )
+	class Phone( enabled: Boolean, template: CharSequence ) extends Validator.Regex( enabled, template, "(\\+?\\d+)?" )
 
-	class Required( enabled: Boolean, message: CharSequence ) extends Validator( enabled, message )
+	class Required( enabled: Boolean, template: CharSequence ) extends Validator( enabled, template )
 	{
 		override def validate( value: CharSequence ) = !enabled || value.length() > 0
 	}
