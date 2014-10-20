c976dcaf15a87cf9936b252485e0cd0fda5c3a93
Niklas Klein <my.taig@gmail.com>
2014-10-20 17:46:50 +0200

    Set project version to 0.0.18-BETA

--- project/Build.scala
+++ project/Build.scala
@@ -23,7 +23,7 @@ object Build extends android.AutoBuild
 				"-language:implicitConversions",
 				"-language:reflectiveCalls"
 			),
-			version := "0.0.17-BETA",
+			version := "0.0.18-BETA",
 			libraryProject in Android := true,
 			minSdkVersion in Android := "10",
 			targetSdkVersion in Android := "21"
