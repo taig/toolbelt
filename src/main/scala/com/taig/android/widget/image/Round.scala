593083e623c6cef37a6d03ae113ccbdb5eaa5bcb
Niklas Klein <my.taig@gmail.com>
2014-10-29 17:14:33 +0100

    Fixed trait Round errors due to recent changes

--- src/main/scala/com/taig/android/widget/image/Round.scala
+++ src/main/scala/com/taig/android/widget/image/Round.scala
@@ -2,12 +2,12 @@ package com.taig.android.widget.image
 
 trait Round extends AspectRatio with Radius
 {
-	ratio.setValue( 1 )
+	setRatio( 1 )
 
 	override def onMeasure( widthMeasure: Int, heightMeasure: Int )
 	{
 		super.onMeasure( widthMeasure, heightMeasure )
 
-		radius.setValue( getMeasuredWidth / 2f )
+		setRadius( getMeasuredWidth / 2f )
 	}
 }
