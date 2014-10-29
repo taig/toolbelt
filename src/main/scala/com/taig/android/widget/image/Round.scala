093a577e5c713eca31ec572b89612c1ad08f26bb
Niklas Klein <my.taig@gmail.com>
2014-10-29 17:17:26 +0100

    Added Round documentation

--- src/main/scala/com/taig/android/widget/image/Round.scala
+++ src/main/scala/com/taig/android/widget/image/Round.scala
@@ -1,5 +1,10 @@
 package com.taig.android.widget.image
 
+/**
+ * ImageView extension that draws its src drawable in a circle
+ * 
+ * This is achieved by using a [[AspectRatio]] of 1, and a [[Radius]] as bis as half a dimension.
+ */
 trait Round extends AspectRatio with Radius
 {
 	setRatio( 1 )
