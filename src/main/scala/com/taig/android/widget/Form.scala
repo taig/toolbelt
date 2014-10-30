53a7dfa093f7bec26e784869c53b6c3d48419222
Niklas Klein <my.taig@gmail.com>
2014-10-30 02:38:42 +0100

    RowSpacing was stupid

--- src/main/scala/com/taig/android/widget/Form.scala
+++ src/main/scala/com/taig/android/widget/Form.scala
@@ -18,46 +18,10 @@ with	Validatable
 
 	private lazy val inputs = findChildren( this )
 
-	private var spacing = 0
-
-	{
-		val array = context.obtainStyledAttributes( attributes, R.styleable.Widget_Form )
-
-		array.getDimensionPixelSize( R.styleable.Widget_Form_rowSpacing, 0 )
-
-		array.recycle()
-	}
-
 	if( getId == View.NO_ID )
 	{
 		setId( R.id.form )
 	}
-
-	apply( spacing, 0 )
-
-	def getRowSpacing = spacing
-
-	def setRowSpacing( spacing: Int )
-	{
-		val previous = this.spacing
-		this.spacing = spacing
-		apply( spacing, previous )
-	}
-
-	private def apply( spacing: Int, previous: Int ) = ( 0 to getChildCount - 1 )
-		.map( getChildAt )
-		.sliding( 2 )
-		.map( _.map( _.getLayoutParams.asInstanceOf[ViewGroup.MarginLayoutParams] ) )
-		.foreach
-		{
-			case Seq( current, next ) =>
-			{
-				if( current.bottomMargin == previous && next.topMargin == 0 )
-				{
-					current.bottomMargin = spacing
-				}
-			}
-		}
 
 	/**
 	 * Recursively find all validatable children of the given view
