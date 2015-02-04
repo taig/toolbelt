706594c62a534ae47f6f4ce474c426b0acd5c281
Niklas Klein <my.taig@gmail.com>
2015-02-04 01:46:41 +0100

    Add missing ClassTag

--- src/main/scala/com/taig/android/content/Contextual.scala
+++ src/main/scala/com/taig/android/content/Contextual.scala
@@ -2,9 +2,11 @@ package com.taig.android.content
 
 import android.content.Context
 
+import scala.reflect.ClassTag
+
 trait Contextual
 {
 	implicit def context: Context
 
-	def startActivity[A]: Unit = context.startActivity( Intent[A] )
+	def startActivity[A: ClassTag]: Unit = context.startActivity( Intent[A] )
 }
