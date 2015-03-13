cf6f8e4f2a3eba6461e42468a36800add7aec301
Niklas Klein <mail@taig.io>
2015-03-13 07:49:35 +0100

    Fix Intent import

--- src/main/scala/com/taig/android/content/Receiver.scala
+++ src/main/scala/com/taig/android/content/Receiver.scala
@@ -1,11 +1,11 @@
 package com.taig.android.content
 
-import android.content.{BroadcastReceiver, Context, Intent}
+import android.content.{BroadcastReceiver, Context}
 
 trait	Receiver
 extends BroadcastReceiver
 {
-	override final def onReceive( context: Context, intent: Intent ) = onReceive( intent )( context )
+	override final def onReceive( context: Context, intent: android.content.Intent ) = onReceive( intent )( context )
 
-	def onReceive( intent: Intent )( implicit context: Context ): Unit
+	def onReceive( intent: android.content.Intent )( implicit context: Context ): Unit
 }
