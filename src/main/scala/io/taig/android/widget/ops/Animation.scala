ab901eaef3414b545423035980ed48e64cb12255
Niklas Klein <mail@taig.io>
2015-06-22 15:54:55 +0200

    Add slideOut animation method

--- src/main/scala/io/taig/android/widget/ops/Animation.scala
+++ src/main/scala/io/taig/android/widget/ops/Animation.scala
@@ -22,21 +22,41 @@ trait Animation
 
 	private def getPosition = Option( view.getTag( R.id.view_initial_position ).asInstanceOf[Point[Int]] )
 
-	def slideIn( direction: Direction, duration: Duration = 350 milliseconds, delay: Duration = 250 milliseconds ) =
-	{
-		// Persist current position on the first call to slide, in order to move back to the initial position after
-		// sliding out
-		val position = getPosition.getOrElse( storePosition() )
+	private def getAndStorePosition = getPosition.getOrElse( storePosition() )
 
+	def slideIn( from: Direction, duration: Duration = 350 milliseconds, delay: Duration = 250 milliseconds ) =
+	{
 		val window = Dimension( view.getContext.WindowManager.getDefaultDisplay )
+		val position = getAndStorePosition
 		val dimension = view.dimension
 
-		val transition = direction match
+		val transition = from match
 		{
 			case Left => new TranslateAnimation( -( position + dimension ).x, 0, 0, 0 )
 			case Top => new TranslateAnimation( 0, 0, -( position + dimension ).y, 0 )
 			case Right => new TranslateAnimation( -( position - window ).x, 0, 0, 0 )
 			case Bottom => new TranslateAnimation( 0, 0, -( position - window ).y, 0 )
+		}
+
+		transition.setStartOffset( delay.toMillis )
+		transition.setDuration( duration.toMillis )
+		transition.setFillAfter( true )
+
+		view.startAnimation( transition )
+	}
+
+	def slideOut( to: Direction, duration: Duration = 350 milliseconds, delay: Duration = 250 milliseconds ) =
+	{
+		val window = Dimension( view.getContext.WindowManager.getDefaultDisplay )
+		val position = getAndStorePosition
+		val dimension = view.dimension
+
+		val transition = to match
+		{
+			case Left => new TranslateAnimation( 0, -( position + dimension ).x, 0, 0 )
+			case Top => new TranslateAnimation( 0, 0, 0, -( position + dimension ).y )
+			case Right => new TranslateAnimation( 0, -( position - window ).x, 0, 0 )
+			case Bottom => new TranslateAnimation( 0, 0, 0, -( position - window ).y )
 		}
 
 		transition.setStartOffset( delay.toMillis )
