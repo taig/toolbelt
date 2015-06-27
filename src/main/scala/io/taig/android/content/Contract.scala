97db8ae963b6b9376e558fd28a5bd97226a204e9
Niklas Klein <mail@taig.io>
2015-06-27 19:42:21 +0200

    Add debtor/credtior pattern

---
+++ src/main/scala/io/taig/android/content/Contract.scala
@@ -0,0 +1,8 @@
+package io.taig.android.content
+
+/**
+ * A contract may be concluded between an Activity (debtor) and a Fragment (creditor)
+ * 
+ * The Fragment defines the contract, the Activity has to implement it
+ */
+trait Contract
