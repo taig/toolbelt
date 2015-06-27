7a2edf2141b5dbed67ae6531ca675a6f264e3753
Niklas Klein <mail@taig.io>
2015-06-27 15:56:34 +0200

    Simplify Ui executor

--- src/main/scala/io/taig/android/concurrent/Executor.scala
+++ src/main/scala/io/taig/android/concurrent/Executor.scala
@@ -3,24 +3,26 @@ package io.taig.android.concurrent
 import android.os.{AsyncTask, Handler, Looper}
 import io.taig.android._
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContextExecutor, ExecutionContext}
 
 object Executor
 {
 	/**
 	 * Android's default asynchronous ExecutionContext
 	 */
-	implicit val Pool: ExecutionContext = ExecutionContext.fromExecutor( AsyncTask.THREAD_POOL_EXECUTOR )
+	implicit val Pool: ExecutionContextExecutor = ExecutionContext.fromExecutor( AsyncTask.THREAD_POOL_EXECUTOR )
 
 	/**
 	 * Ui-thread ExecutionContext
 	 */
-	val Ui: ExecutionContext = ExecutionContext.fromExecutor( new java.util.concurrent.Executor
+	val Ui = new ExecutionContext
 	{
 		private val handler = new Handler( Looper.getMainLooper )
 
+		override def reportFailure( cause: Throwable ) = cause.printStackTrace()
+
 		override def execute( command: Runnable ) = handler.post( command )
-	} )
+	}
 
 	/**
 	 * Run on the Ui-Thread
