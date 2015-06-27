cca0a019c7a1deb1d8cc59d9fc6c552ad2dc0520
Niklas Klein <mail@taig.io>
2015-06-27 20:14:21 +0200

    The classtag does not provide any benfit here

--- src/main/scala/io/taig/android/content/Creditor.scala
+++ src/main/scala/io/taig/android/content/Creditor.scala
@@ -1,7 +1,32 @@
 package io.taig.android.content
 
+import android.os.Bundle
+
 /**
  * A Fragment may be a Creditor, forcing the hosting Activity to implement its Contract
  */
 trait	Creditor[+C]
-extends	internal.Creditor[C]
+extends	android.app.Fragment
+{
+	def debtor: C = getActivity.asInstanceOf[C]
+
+	override def onActivityCreated( state: Bundle ) =
+	{
+		super.onActivityCreated( state )
+
+		// It is okay to do the ugly cast try here, because there is nothing to gain with a ClassTag. It would enforce
+		// us to implement a field in the child without providing any actual benefit. This is intended as debugging
+		// message during the development process.
+		try
+		{
+			debtor
+		}
+		catch
+		{
+			case _: ClassCastException =>
+			{
+				sys.error( getActivity.getClass.getName + " has to implement the Contract" )
+			}
+		}
+	}
+}
