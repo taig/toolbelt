e4ef01dc9002c74e8564686b96c4bdabc87c14a5
Niklas Klein <mail@taig.io>
2015-08-04 01:14:02 +0200

    Split log messages; fck you, logcat

--- src/main/scala/io/taig/android/util/Log.scala
+++ src/main/scala/io/taig/android/util/Log.scala
@@ -4,29 +4,34 @@ import android.{ util => android }
 
 object Log
 {
-	def d( message: Any )( implicit tag: Tag ) = android.Log.d( tag.name, message.toString )
+	def d( message: Any )( implicit tag: Tag ) = print( message, android.Log.d( tag.name, _ ) )
 
-	def d( message: Any, error: Throwable )( implicit tag: Tag ) = android.Log.d( tag.name, message.toString, error )
+	def d( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, android.Log.d( tag.name, _, error ) )
 
-	def e( message: Any )( implicit tag: Tag ) = android.Log.e( tag.name, message.toString )
+	def e( message: Any )( implicit tag: Tag ) = print( message, android.Log.e( tag.name, _ ) )
 
-	def e( message: Any, error: Throwable )( implicit tag: Tag ) = android.Log.e( tag.name, message.toString, error )
+	def e( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, android.Log.e( tag.name, _, error ) )
 
-	def i( message: Any )( implicit tag: Tag ) = android.Log.i( tag.name, message.toString )
+	def i( message: Any )( implicit tag: Tag ) = print( message, android.Log.i( tag.name, _ ) )
 
-	def i( message: Any, error: Throwable )( implicit tag: Tag ) = android.Log.i( tag.name, message.toString, error )
+	def i( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, android.Log.i( tag.name, _, error ) )
 
-	def v( message: Any )( implicit tag: Tag ) = android.Log.v( tag.name, message.toString )
+	def v( message: Any )( implicit tag: Tag ) = print( message, android.Log.v( tag.name, _ ) )
 
-	def v( message: Any, error: Throwable )( implicit tag: Tag ) = android.Log.v( tag.name, message.toString, error )
+	def v( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, android.Log.v( tag.name, _, error ) )
 
-	def w( message: Any )( implicit tag: Tag ) = android.Log.w( tag.name, message.toString )
+	def w( message: Any )( implicit tag: Tag ) = print( message, android.Log.w( tag.name, _ ) )
 
-	def w( message: Any, error: Throwable )( implicit tag: Tag ) = android.Log.w( tag.name, message.toString, error )
+	def w( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, android.Log.w( tag.name, _, error ) )
 
-	def wtf( message: Any )( implicit tag: Tag ) = android.Log.wtf( tag.name, message.toString )
+	def wtf( message: Any )( implicit tag: Tag ) = print( message, android.Log.wtf( tag.name, _ ) )
 
-	def wtf( message: Any, error: Throwable )( implicit tag: Tag ) = android.Log.wtf( tag.name, message.toString, error )
+	def wtf( message: Any, error: Throwable )( implicit tag: Tag ) =
+	{
+		print( message, android.Log.wtf( tag.name, _, error ) )
+	}
+
+	private def print( message: Any, log: String => Unit ) = message.toString.grouped( 1000 ).foreach( log )
 
 	case class	Tag( name: String )
 	extends		AnyVal
