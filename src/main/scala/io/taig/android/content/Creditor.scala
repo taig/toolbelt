5beae24a4e0a83fdf2f6e89fb70aaffb44b153d0
Niklas Klein <mail@taig.io>
2015-08-06 19:44:24 +0200

    Save activity context for creditors in onAttach

--- src/main/scala/io/taig/android/content/Creditor.scala
+++ src/main/scala/io/taig/android/content/Creditor.scala
@@ -3,12 +3,28 @@ package io.taig.android.content
 /**
  * A Fragment may be a Creditor, forcing the hosting Activity to implement its contract
  */
-trait	Creditor[+C]
+trait	Creditor[C]
 extends	Fragment
 {
+	private var activity: android.app.Activity = null
+
 	// It would be wise to check if the Activity implements C in onActivityCreated(), but that would enforce us to
 	// implement an implicit classTag field in the children. This might work out in the future with trait parameters.
 	// Until then, please implement the fucking contract.
 
-	def debtor: C = context.asInstanceOf[C]
+	override def onAttach( activity: android.app.Activity ) =
+	{
+		super.onAttach( activity )
+
+		this.activity = activity
+	}
+
+	override def onDetach( ) =
+	{
+		super.onDetach()
+
+		this.activity = null
+	}
+
+	def debtor: C = activity.asInstanceOf[C]
 }
