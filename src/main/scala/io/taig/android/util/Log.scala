33abae91323079e237708554353544ba03935b4b
Niklas Klein <mail@taig.io>
2015-08-20 10:29:48 +0200

    Split long exceptions into multiple logs

--- src/main/scala/io/taig/android/util/Log.scala
+++ src/main/scala/io/taig/android/util/Log.scala
@@ -1,27 +1,28 @@
 package io.taig.android.util
 
+import android.util.Log.getStackTraceString
 import android.{ util ⇒ android }
 
 object Log {
     def d( message: Any )( implicit tag: Tag ) = print( message, android.Log.d( tag.name, _ ) )
 
-    def d( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, android.Log.d( tag.name, _, error ) )
+    def d( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, error, android.Log.d( tag.name, _ ) )
 
     def e( message: Any )( implicit tag: Tag ) = print( message, android.Log.e( tag.name, _ ) )
 
-    def e( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, android.Log.e( tag.name, _, error ) )
+    def e( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, error, android.Log.e( tag.name, _ ) )
 
     def i( message: Any )( implicit tag: Tag ) = print( message, android.Log.i( tag.name, _ ) )
 
-    def i( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, android.Log.i( tag.name, _, error ) )
+    def i( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, error, android.Log.i( tag.name, _ ) )
 
     def v( message: Any )( implicit tag: Tag ) = print( message, android.Log.v( tag.name, _ ) )
 
-    def v( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, android.Log.v( tag.name, _, error ) )
+    def v( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, error, android.Log.v( tag.name, _ ) )
 
     def w( message: Any )( implicit tag: Tag ) = print( message, android.Log.w( tag.name, _ ) )
 
-    def w( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, android.Log.w( tag.name, _, error ) )
+    def w( message: Any, error: Throwable )( implicit tag: Tag ) = print( message, error, android.Log.w( tag.name, _ ) )
 
     def wtf( message: Any )( implicit tag: Tag ) = print( message, android.Log.wtf( tag.name, _ ) )
 
@@ -30,6 +31,10 @@ object Log {
     }
 
     private def print( message: Any, log: String ⇒ Unit ) = message.toString.grouped( 1000 ).foreach( log )
+
+    private def print( message: Any, throwable: Throwable, log: String ⇒ Unit ) = {
+        ( message + "\n" + getStackTraceString( throwable ) ).grouped( 1000 ).foreach( log )
+    }
 
     case class Tag( name: String )
         extends AnyVal
