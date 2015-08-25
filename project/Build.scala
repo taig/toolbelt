e9a2b31c0120f4f7b4c9f4cbc037c521c59791d7
Niklas Klein <mail@taig.io>
2015-08-25 11:47:29 +0200

    Extract sonatype settings

--- project/Build.scala
+++ project/Build.scala
@@ -2,12 +2,11 @@ import sbt._
 import sbt.Keys._
 import android.Keys._
 import android.Plugin._
-import xerial.sbt.Sonatype._
 
 object	Build
 extends	sbt.Build
 {
-	lazy val main = Project( "toolbelt", file( "." ), settings = androidBuildAar ++ sonatypeSettings )
+	lazy val main = Project( "toolbelt", file( "." ), settings = androidBuildAar )
 		.settings(
 			javacOptions ++= (
 				"-source" :: "1.7" ::
@@ -40,50 +39,5 @@ extends	sbt.Build
 			platformTarget in Android := "android-23",
 			targetSdkVersion in Android := "23",
 			typedResources in Android := false
-		)
-		.settings(
-			description := "Essential helpers for Scala on Android",
-			homepage := Some( url( "https://github.com/taig/toolbelt" ) ),
-			licenses := Seq( "MIT" -> url( "https://raw.githubusercontent.com/taig/toolbelt/master/LICENSE" ) ),
-			organizationHomepage := Some( url( "http://taig.io" ) ),
-			pomExtra :=
-			{
-				<issueManagement>
-					<url>https://github.com/taig/toolbelt/issues</url>
-					<system>GitHub Issues</system>
-				</issueManagement>
-				<developers>
-					<developer>
-						<id>Taig</id>
-						<name>Niklas Klein</name>
-						<email>mail@taig.io</email>
-						<url>http://taig.io/</url>
-					</developer>
-				</developers>
-			},
-			pomIncludeRepository := { _ => false },
-			publishArtifact in Test := false,
-			publishMavenStyle := true,
-			publishTo <<= version ( version =>
-			{
-				val url = Some( "https://oss.sonatype.org/" )
-
-				if( version.endsWith( "SNAPSHOT" ) )
-				{
-					url.map( "snapshot" at _ + "content/repositories/snapshots" )
-				}
-				else
-				{
-					url.map( "release" at _ + "service/local/staging/deploy/maven2" )
-				}
-			} ),
-			scmInfo := Some(
-				ScmInfo(
-					url( "https://github.com/taig/toolbelt" ),
-					"scm:git:git://github.com/taig/toolbelt.git",
-					Some( "scm:git:git@github.com:taig/toolbelt.git" )
-				)
-			),
-			startYear := Some( 2014 )
 		)
 }
