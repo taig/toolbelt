f9fd598218e464c3e154aef292e0efec2925ac53
Niklas Klein <mail@taig.io>
2015-09-11 21:00:22 +0200

    Add dialog disappears hotfix

--- src/main/scala/io/taig/android/content/Dialog.scala
+++ src/main/scala/io/taig/android/content/Dialog.scala
@@ -1,16 +1,19 @@
 package io.taig.android.content
 
-import android.app
 import android.os.Bundle
 
 trait Dialog
         extends android.app.DialogFragment
         with Fragment {
-    override final def onCreateDialog( state: Bundle ) = {
-        super.onCreateDialog( state )
+    override final def onCreateDialog( state: Bundle ) = onCreateDialog( Option( state ) )
 
-        onCreateDialog( Option( state ) )
+    def onCreateDialog( state: Option[Bundle] ): android.app.Dialog = new android.app.Dialog( getActivity, getTheme )
+
+    override def onDestroyView() = {
+        if ( getDialog != null && getRetainInstance ) {
+            getDialog.setDismissMessage( null )
+        }
+
+        super.onDestroyView()
     }
-
-    def onCreateDialog( state: Option[Bundle] ): app.Dialog = new app.Dialog( getActivity, getTheme )
 }
