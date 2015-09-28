8732bfd9ed0383b98d073de9bdea9626eeb047dd
Niklas Klein <mail@taig.io>
2015-09-28 15:53:36 +0200

    Add bundle operations

--- src/main/scala/io/taig/android/extension/util/package.scala
+++ src/main/scala/io/taig/android/extension/util/package.scala
@@ -2,12 +2,16 @@ package io.taig.android.extension
 
 import java.net.URLEncoder
 
+import android.os.Bundle
 import io.taig.android.util.Log
+import io.taig.android.util.operation
 import io.taig.android.util.Log.Tag
 
 import scala.language.implicitConversions
 
 package object util {
+    implicit class ToolbeltBundle( bundle: Bundle ) extends operation.Bundle( bundle )
+
     implicit class ToolbeltString( string: String ) {
         def encode( charset: String = "UTF-8" ) = URLEncoder.encode( string, charset )
     }
