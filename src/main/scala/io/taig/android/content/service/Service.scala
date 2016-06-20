a0a9766c27ba9023de2f3bf976f8a4987cc43b64
Niklas Klein <mail@taig.io>
2016-06-20 16:42:16 +0200

    Add Service.isRunning method

--- src/main/scala/io/taig/android/content/service/Service.scala
+++ src/main/scala/io/taig/android/content/service/Service.scala
@@ -1,9 +1,28 @@
 package io.taig.android.content.service
 
+import android.app.ActivityManager
+import android.content.Context
 import io.taig.android.content.Contextual
+import io.taig.android.extension.content._
+
+import scala.reflect._
 
 trait Service
         extends android.app.Service
         with Contextual {
     override implicit def context = getApplicationContext
+}
+
+object Service {
+    abstract class Companion[S <: android.app.Service: ClassTag] {
+        def isRunning( implicit c: Context ): Boolean = {
+            import collection.JavaConversions._
+
+            val target = classTag[S].runtimeClass.getName
+
+            c.service[ActivityManager]
+                .getRunningServices( Int.MaxValue )
+                .exists( _.service.getClassName == target )
+        }
+    }
 }
