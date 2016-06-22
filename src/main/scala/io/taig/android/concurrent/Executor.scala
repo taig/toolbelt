2c1b8ac7750799e037cd17c51b69d15f95a52d32
Niklas Klein <mail@taig.io>
2016-06-22 16:24:45 +0200

    Refactor ExecutionContexts to Schedulers

--- src/main/scala/io/taig/android/concurrent/Executor.scala
+++ src/main/scala/io/taig/android/concurrent/Executor.scala
@@ -1,41 +1,96 @@
 package io.taig.android.concurrent
 
+import java.util.concurrent.TimeUnit
+
 import android.os.{ AsyncTask, Handler, Looper }
 import io.taig.android._
 import io.taig.android.util.Log
+import monix.execution.schedulers.ExecutionModel.SynchronousExecution
+import monix.execution.{ Cancelable, Scheduler }
 
-import scala.concurrent.{ ExecutionContextExecutor, ExecutionContext }
+import scala.concurrent.ExecutionContext
 
 object Executor {
     /**
-     * Android's default asynchronous ExecutionContext
+     * Pool thread Scheduler
      */
-    implicit val Pool: ExecutionContextExecutor = ExecutionContext.fromExecutor(
-        AsyncTask.THREAD_POOL_EXECUTOR,
-        cause ⇒ Log.e( cause.getMessage, cause )( Log.Tag( Pool.getClass.getName ) )
-    )
+    implicit val Pool: Scheduler = Scheduler {
+        ExecutionContext.fromExecutor(
+            AsyncTask.THREAD_POOL_EXECUTOR,
+            exception ⇒ report( exception, Log.Tag( Pool.getClass.getName ) )
+        )
+    }
 
     /**
-     * Single thread execution context
+     * Single thread Scheduler
      */
-    val Single: ExecutionContext = ExecutionContext.fromExecutor(
-        AsyncTask.SERIAL_EXECUTOR,
-        cause ⇒ Log.e( cause.getMessage, cause )( Log.Tag( Single.getClass.getName ) )
-    )
+    val Single: Scheduler = Scheduler {
+        ExecutionContext.fromExecutor(
+            AsyncTask.SERIAL_EXECUTOR,
+            exception ⇒ report( exception, Log.Tag( Single.getClass.getName ) )
+        )
+    }
 
     /**
-     * Ui-thread ExecutionContext
+     * Ui thread Scheduler
      */
-    val Ui: ExecutionContext = new ExecutionContext {
+    val Ui: Scheduler = new Scheduler {
         val handler = new Handler( Looper.getMainLooper )
 
-        override def reportFailure( cause: Throwable ) = {
-            Log.e( cause.getMessage, cause )( Log.Tag( Ui.getClass.getName ) )
+        override def reportFailure( exception: Throwable ) = report( exception, Log.Tag( Ui.getClass.getName ) )
+
+        override def execute( runnable: Runnable ) = handler.post( runnable )
+
+        override def currentTimeMillis() = System.currentTimeMillis()
+
+        override val executionModel = SynchronousExecution
+
+        override def scheduleOnce( delay: Long, unit: TimeUnit, runnable: Runnable ) = {
+            handler.postDelayed( runnable, unit.toMillis( delay ) )
+            Cancelable.empty
         }
 
-        override def execute( command: Runnable ) = handler.post( command )
+        override def scheduleAtFixedRate( delay: Long, period: Long, unit: TimeUnit, runnable: Runnable ) = {
+            val delayMillis = unit.toMillis( delay )
+            val periodMillis = unit.toMillis( period )
 
-        def execute( command: Runnable, delay: Long ) = handler.postDelayed( command, delay )
+            var active = true
+
+            handler.postDelayed(
+                keepSchedulingAtFixedRate( runnable, periodMillis, active == false ),
+                delayMillis
+            )
+
+            Cancelable( () ⇒ active = false )
+        }
+
+        def keepSchedulingAtFixedRate( runnable: Runnable, period: Long, isCanceled: ⇒ Boolean ): Runnable = () ⇒ {
+            if ( !isCanceled ) {
+                handler.postDelayed( keepSchedulingAtFixedRate( runnable, period, isCanceled ), period )
+                runnable.run()
+            }
+        }
+
+        override def scheduleWithFixedDelay( delay: Long, period: Long, unit: TimeUnit, runnable: Runnable ) = {
+            val delayMillis = unit.toMillis( delay )
+            val periodMillis = unit.toMillis( period )
+
+            var active = true
+
+            handler.postDelayed(
+                keepSchedulingWithFixedDelay( runnable, periodMillis, active == false ),
+                delayMillis
+            )
+
+            Cancelable( () ⇒ active = false )
+        }
+
+        def keepSchedulingWithFixedDelay( runnable: Runnable, period: Long, isCanceled: ⇒ Boolean ): Runnable = () ⇒ {
+            if ( !isCanceled ) {
+                runnable.run()
+                handler.postDelayed( keepSchedulingWithFixedDelay( runnable, period, isCanceled ), period )
+            }
+        }
     }
 
     /**
@@ -43,5 +98,9 @@ object Executor {
      */
     def Ui( body: ⇒ Unit ): Unit = Ui.execute( body )
 
-    def Ui( body: ⇒ Unit, delay: Long ): Unit = Ui.execute( body, delay )
+    def Ui( body: ⇒ Unit, delay: Long ): Unit = Ui.scheduleOnce( delay, TimeUnit.MILLISECONDS, body )
+
+    private def report( exception: Throwable, tag: Log.Tag ): Unit = {
+        Log.e( "Failure during asynchronous operation", exception )( tag )
+    }
 }
