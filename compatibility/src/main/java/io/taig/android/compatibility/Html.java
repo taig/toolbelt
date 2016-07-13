24488283a54dd8a131d757791e5c587400b510d9
Niklas Klein <mail@taig.io>
2016-07-13 16:35:52 +0200

    Add Html.fromHtml compat method

---
+++ compatibility/src/main/java/io/taig/android/compatibility/Html.java
@@ -0,0 +1,19 @@
+package io.taig.android.compatibility;
+
+import static android.text.Html.FROM_HTML_MODE_LEGACY;
+import android.text.Spanned;
+
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.N;
+
+public class Html {
+    @SuppressWarnings( "deprecation" )
+    public static Spanned fromHtml( String source ) {
+        if( SDK_INT >= N ) {
+            return android.text.Html.fromHtml( source, FROM_HTML_MODE_LEGACY );
+        } else {
+            return android.text.Html.fromHtml( source );
+        }
+        
+    }
+}
