28f684a6f62d892a50dc86af49c66be0afa4c2c2
Niklas Klein <mail@taig.io>
2016-11-10 15:06:50 +0100

    Upgrade to cats 0.8.1

--- project/Settings.scala
+++ project/Settings.scala
@@ -35,7 +35,7 @@ object Settings {
 
         val androidSupportV13 = "com.android.support" % "support-v13" % androidSupport
 
-        val cats = "0.7.2"
+        val cats = "0.8.1"
 
         val monix = "2.1.0"
 
