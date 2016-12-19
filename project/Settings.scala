ed8a35abfee24d49e43bf6bea409707560113c18
Niklas Klein <mail@taig.io>
2016-12-19 12:06:57 +0100

    Upgrade to play services 10.0.1

--- project/Settings.scala
+++ project/Settings.scala
@@ -43,7 +43,7 @@ object Settings {
 
         val monixReactive = "io.monix" %% "monix-reactive" % monix
         
-        val playServices = "9.8.0"
+        val playServices = "10.0.1"
 
         val playServicesBase = "com.google.android.gms" % "play-services-base" % playServices
 
