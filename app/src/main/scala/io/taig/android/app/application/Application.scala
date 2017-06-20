e368720a8af91f172c9410661ff1640772e5ef72
Niklas Klein <mail@taig.io>
2017-06-20 12:17:48 +0200

    Use more specific context type

--- app/src/main/scala/io/taig/android/app/application/Application.scala
+++ app/src/main/scala/io/taig/android/app/application/Application.scala
@@ -5,5 +5,5 @@ import io.taig.android.context.Contextual
 trait Application
         extends android.app.Application
         with Contextual {
-    override implicit def context: android.app.Application = this
+    override implicit def context: Application = this
 }
